3548c57585167f4c719eb11d2533a31a854f4d9d
tysker <joergoertel@proton.me>
2024-05-07 18:05:13 +0200

    test deployment

--- src/main/java/dk/lyngby/config/ApplicationConfig.java
+++ src/main/java/dk/lyngby/config/ApplicationConfig.java
@@ -31,7 +31,7 @@ public class ApplicationConfig {
     }
 
     public static void corsConfig(Context ctx) {
-        ctx.header("Access-Control-Allow-Origin", "*");
+        ctx.header("Access-Control-Allow-Origin", System.getenv("FRONTEND_URL") !=null ? System.getenv("FRONTEND_URL") : "*");
         ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD");
         ctx.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
         ctx.header("Access-Control-Allow-Credentials", "true");
